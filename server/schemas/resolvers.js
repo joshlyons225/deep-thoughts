59d5c3578c584c670178b7a8e139e12ac9136ad5
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-05-03 14:01:39 -0500

    updated defs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,32 @@
+// import models
+const { User, Thought } = require("../models");
+
+// set Thought query resolver
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return "Hello world!";
+    // search by Thoughts by username
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      // return results in descending order
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    // search Thoughts by id
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+    // get all users
+    users: async () => {
+      return User.find()
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
+    // get a user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
     },
   },
 };
